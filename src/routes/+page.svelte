d49b0c25760302352ce88c554cfdeb3738fb9529
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-12-29 20:42:48 +0530

    services section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,61 @@
-<hero class="flex w-full items-center md:h-screen flex-col">
-    <img src="/home/hero-img.png" class="h-3/5 md:w-full object-cover" alt="A guy typing in laptop keyboard"/>
-    <div class="max-w-screen-xl p-5">
-        <p class="lg:text-2xl text-lg lg:text-center xl:text-justify">At Apex Research and Scientific Writing Academy, we provide premier scientific alteration services encompassing all components of scientific dialogue, including comprehensive research papers, financial aid proposal drafts, medical correspondence strategies, and instructive manuals. Our adept scientific revision experts collaborate closely with you to generate a clear, well-structured document that eloquently conveys your scientific, health, and technical research findings, guaranteeing publication in English-speaking journals, securing successful fund appropriations.</p>
+<hero class="flex w-full items-center flex-col">
+    <img src="/home/hero-img.png" class="h-3/5 sm:w-full object-cover" alt="A guy typing in laptop keyboard"/>
+    <div class="xl:w-3/4 p-5">
+        <p class="lg:text-2xl md:text-xl text-sm text-justify text-slate-700 font-medium">At Apex Research and Scientific Writing Academy, we provide premier scientific alteration services encompassing all components of scientific dialogue, including comprehensive research papers, financial aid proposal drafts, medical correspondence strategies, and instructive manuals. Our adept scientific revision experts collaborate closely with you to generate a clear, well-structured document that eloquently conveys your scientific, health, and technical research findings, guaranteeing publication in English-speaking journals, securing successful fund appropriations.</p>
+        <hr class="border-2 border-slate-400 my-5 rounded-lg"/>
     </div>
 </hero>
+
+<section class="w-full flex justify-center" id="about">
+    <div class="flex xl:w-3/4 w-full h-full">
+        <div class="w-full flex justify-center items-center lg:flex-row flex-col">
+            <img src="/home/about_us.jpg" alt="A guy and a girl speaking" class="lg:w-3/4 w-full object-cover z-0"/>
+            <div class="border border-purple-700 lg:shadow-[rgba(126,_34,_206,_0.4)_0px_60px_40px_-7px] p-5 sm:w-[400px] sm:h-[400px] lg:-translate-x-40 bg-white z-10 lg:m-0 m-5">
+                <h2 class="text-2xl text-purple-700 font-bold text-center">About Us</h2>
+                <p class="text-slate-800 text-xl">At Apex Research and Scientific Writing Academy, experienced professors and scientists guide you with expert mentorship. Our programs blend theory, practical exercises, and personalized support to empower your research and communication skills. Unlock your potential and succeed in the dynamic world of academia and science. Join us for a journey of growth and excellence</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="w-full flex flex-col justify-center items-center mt-5" id="services">
+    <div class="p-5 border border-slate-400 bg-slate-300 w-full">
+        <h2 class="text-center text-purple-700 font-medium md:text-3xl text-xl">Content we refine</h2>
+    </div>
+<div class="xl:w-3/4 flex justify-center bg-slate-300 mt-5">
+    <div class="lg:w-3/4 sm:p-5 p-2 flex flex-col justify-center items-center">
+        <div class="bg-white sm:p-5 p-1 flex md:flex-row flex-col justify-center items-center my-5">
+            <img src="/services/im2.png" alt="book" class="w-40"/>
+            <div class="sm:p-5">
+                <h4 class="text-slate-800 italic text-2xl font-medium text-center">Research Papers</h4>
+                <p class="text-xl">Our specialty is refining various scientific papers for peer-reviewed journal publication, guaranteeing clear communication, logical flow, exactness, and lucid data visualization. </p>
+            </div>
+        </div>
+        <div class="bg-white sm:p-5 p-1 flex md:flex-row flex-col justify-center items-center my-5">
+            <img src="/services/im4.png" alt="book" class="w-40"/>
+            <div class="sm:p-5">
+                <h4 class="text-slate-800 italic text-2xl font-medium text-center">Theses and Dissertations</h4>
+                <p class="text-xl">Our thesis and dissertation editing services will ensure clarity of presentation, logical structure, coherence of arguments, and referencing and citation accuracy.</p>
+            </div>
+        </div>
+        <div class="bg-white sm:p-5 p-1 flex md:flex-row flex-col justify-center items-center my-5">
+            <img src="/services/im5.png" alt="book" class="w-40"/>
+            <div class="sm:p-5">
+                <h4 class="text-slate-800 italic text-2xl font-medium text-center">Conference papers and abstracts</h4>
+                <p class="text-xl">We examine and polish symposium documents and summaries, confirming they are clear, succinct, and in line with submission criteria.</p>
+            </div>
+        </div>
+        <div class="self-start">
+            <button class="bg-slate-200 text-slate-950 border border-slate-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
+              <span class="bg-slate-400 shadow-slate-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+              Learn More
+            </button>
+        </div>
+
+    </div>
+    <div class="flex-col justify-center p-5 lg:flex hidden">
+        <img src="/services/im1.png" alt="feather pen " class="object-cover"/>
+        <img src="/services/im3.png" alt="feather pen " class="object-cover"/>
+    </div>
+</div>
+</section>
